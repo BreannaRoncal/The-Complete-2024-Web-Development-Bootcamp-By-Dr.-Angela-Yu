e78cff5558e86515b078f91023c0644e3e444ab2
Breanna Roncal <36829789+BreannaRoncal@users.noreply.github.com>
2024-02-05 16:00:12 -0800

    Update index.js

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,10 +1,16 @@
 function playGame() {
+  // Get a random number 0 - 5 for player 1
   var p1 = Math.floor(Math.random() * 6);
+  // Change the dice image shown on the website based on the random number
+  // display ./images/dice1.png to ./images/dice6.png
   document.querySelector(".img1").src = "./images/dice" + (p1 + 1) + ".png";
 
+  // Do the same for player 2
+  // I need to make a new Math.random() seed for player 2, otherwise the dice result will be the same
   var p2 = Math.floor(Math.random() * 6);
   document.querySelector(".img2").src = "./images/dice" + (p2 + 1) + ".png";
 
+  // Declare a winner based on which player rolled the highest and display it on the website
   if (p1 > p2) {
     document.querySelector("h1").textContent = "Player 1 Wins!";
   } else if (p1 < p2) {
