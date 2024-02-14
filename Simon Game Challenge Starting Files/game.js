16255b2a0d61d1238d9d644c6a073553b8444e95
Breanna Roncal <36829789+BreannaRoncal@users.noreply.github.com>
2024-02-14 12:18:04 -0800

    Update game.js
    Generate a random color to add to the game pattern

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,11 @@
-alert("hello");
+var gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+var randomChosenColor = buttonColors[nextSequence()];
+
+gamePattern.push(randomChosenColor);
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  return randomNumber;
+}
+
