876183ff4425cf53d94f16aceea1f7d4d8169e37
Breanna Roncal <36829789+BreannaRoncal@users.noreply.github.com>
2024-02-15 15:34:03 -0800

    Update game.js

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,24 +1,39 @@
-var gamePattern = [];
-var userClickedPattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
-var randomChosenColor = buttonColors[nextSequence()];
 
-gamePattern.push(randomChosenColor);
+// Keep track of the randomly created pattern
+var gamePattern = [];
 
-$(".btn").on("click", function () {
-  var userChosenColor = this.id;
+// Keep track of the user's pattern
+var userClickedPattern = [];
+
+
+$(".btn").click(function () {
+  var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
-  clickButton(userChosenColor);
+  playSound(userChosenColor);
 });
 
+// Select a random color, make it flash, and play its sound
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
 }
 
-function clickButton(randomChosenColor) {
-  $("#" + randomChosenColor).addClass("pressed");
+function playSound(name) {
+  var sound = new Audio("sounds/" + name + ".mp3");
+  sound.play();
+}
+
+// When the user clicks the button, simulate it being pressed
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
-    $("#" + randomChosenColor).removeClass("pressed");
+    $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
