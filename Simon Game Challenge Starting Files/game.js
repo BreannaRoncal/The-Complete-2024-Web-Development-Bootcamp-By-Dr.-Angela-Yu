8d8e1718ec79f1c2829900db2151efbcb222dcda
Breanna Roncal <36829789+BreannaRoncal@users.noreply.github.com>
2024-02-15 15:06:10 -0800

    Update game.js
    - Keep track of user's clicked colors
    - Create a handler to add the press effect on the user's clicked colors

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,11 +1,24 @@
 var gamePattern = [];
+var userClickedPattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 var randomChosenColor = buttonColors[nextSequence()];
 
 gamePattern.push(randomChosenColor);
+
+$(".btn").on("click", function () {
+  var userChosenColor = this.id;
+  userClickedPattern.push(userChosenColor);
+  clickButton(userChosenColor);
+});
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   return randomNumber;
 }
 
+function clickButton(randomChosenColor) {
+  $("#" + randomChosenColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + randomChosenColor).removeClass("pressed");
+  }, 100);
+}
