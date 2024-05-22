4e681a6c0c4ab0ecba15da0dd2844c9bb25d1344
Breanna Roncal <36829789+BreannaRoncal@users.noreply.github.com>
2024-05-22 12:08:26 -0700

    Create index.js

---
+++ React/Intro to JSX/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> What I Did Today</h1>
+    <ul>
+      <li>Exercise</li>
+      <li>Brawl Stars</li>
+      <li>Code</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
